dfe13e897d6c11db924b8843dcc3a6a61de2d5da
SaiSatya16 <jj1039@srmist.edu.in>
2023-10-03 03:40:00 +0530

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -24,6 +24,35 @@ def get_allposts():
         data.append({'id': post.id, 'title': post.title, 'content': post.content})
     return data
     # return render_template('allposts.html', posts=posts)
+
+@app.route('/add_post', methods=['GET', 'POST'])
+def add_post():
+    if request.method == 'POST':
+        data = request.get_json()
+        print(data.get('title')), print(data.get('content'))
+        blog = Blog(title=data.get('title', None), content=data.get('content', None))
+        db.session.add(blog)
+        db.session.commit()
+        return  jsonify({'message': 'success'})
+    #     title = request.form['title']
+    #     content = request.form['content']
+
+    #     post = Blog(title=title, content=content)
+    #     db.session.add(post)
+    #     db.session.commit()
+    #     return redirect(url_for('get_allposts'))
+    # return render_template('add_post.html')
+
+#route to delete a post
+@app.route('/delete_post/<int:id>')
+def delete_post(id):
+    post = Blog.query.get(id)
+    db.session.delete(post)
+    db.session.commit()
+    return jsonify({'message': 'success'})
+
+    
+
 
 if __name__ == '__main__':
     # with app.app_context():
--- static/vue/components/post.js
+++ static/vue/components/post.js
@@ -2,12 +2,12 @@ const Post = Vue.component('post', {
     template: `<div>
             <h1>Post</h1>
                 <div class = "row">
-                    <div class="card my-3 mx-3 col-3" style="width: 18rem;" v-for=" post in posts">
+                    <div class="card my-4 mx-4 col-4" style="width: 18rem;" v-for=" post in posts">
                         <div class="card-body">
                         <h5 class="card-title">{{post.title}}</h5>
                         <h6 class="card-subtitle mb-2 text-body-secondary">Post Id: {{post.id}}</h6>
                         <p class="card-text">{{post.content}}</p>
-                        <a href="#" class="card-link">delete post</a>
+                        <button @click="delete_blog(post.id)" class="card-link">delete post</button>
                         </div>
                 </div>
             </div>
@@ -25,11 +25,22 @@ const Post = Vue.component('post', {
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                            ...
+                            <div class= "my-3">
+                                <label for="title"> Enter Post Title</label>
+                                <input v-model = "title" type="text" id="title" class="form-control" placeholder="Title">
+                            
+                            </div>
+                            <div class="my-3">
+
+                                <label for="Content"> Enter Post Content</label>
+                                <input v-model = "content" type="text" id="Content" class="form-control" placeholder="Content">
+                                
+                            
+                            </div>
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Submit</button>
+                            <button type="button" @click="addblog" class="btn btn-primary" data-bs-dismiss="modal" >Submit</button>
                         </div>
                         </div>
                     </div>
@@ -38,15 +49,60 @@ const Post = Vue.component('post', {
         data: function() {
             return {
                 posts: [],
+                title: "",
+                content: "",
+
             }
+        },
+        methods: {
+            addblog: function(){
+                console.log(this.title);
+                console.log(this.content);
+                fetch("/add_post", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        title: this.title,
+                        content: this.content,
+                    }),
+                }).then(response => response.json()).then(data => {
+                    console.log(data);
+                    fetch("/get_allposts").then(response => response.json()).then(data => {
+                        console.log(data);
+                        this.posts = data;
+                    })
+
+                    // this.posts.push(data);
+                    // this.title = "";
+                    // this.content = "";
+                    // // this.$router.go(0);
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+            },
+            delete_blog : function(id){
+                fetch(`/delete_post/${id}`).then(r => r.json()).then(d => {
+                    console.log(d);
+                    fetch("/get_allposts").then(response => response.json()).then(data => {
+                        console.log(data);
+                        this.posts = data;
+                    })
+                
+                });
+            }
+
         },
         mounted : function(){
         document.title = "Post";
         fetch("/get_allposts").then(response => response.json()).then(data => {
             console.log(data);
             this.posts = data;
-        })
-    }
+        });
+    
+    },
 });
 
 export default Post;

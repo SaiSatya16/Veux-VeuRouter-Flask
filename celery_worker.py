df039ed611c67b4fa9bc027d513d06c167adc2ca
jj1039 <jj1039@srmist.edu.in>
2023-12-16 00:48:34 +0530

    update

--- celery_worker.py
+++ celery_worker.py
@@ -1,17 +1,17 @@
 from celery import Celery
 
 def make_celery(app):
-    celery = Celery(
+    celery_app = Celery(
         "app",
         backend=app.config['CELERY_RESULT_BACKEND'],
         broker=app.config['CELERY_BROKER_URL']
     )
-    celery.conf.update(app.config)
+    celery_app.conf.update(app.config)
 
-    class ContextTask(celery.Task):
+    class ContextTask(celery_app.Task):
         def __call__(self, *args, **kwargs):
             with app.app_context():
                 return self.run(*args, **kwargs)
 
-    celery.Task = ContextTask
-    return celery
+    celery_app.Task = ContextTask
+    return celery_app
--- static/vue/components/post.js
+++ static/vue/components/post.js
@@ -4,76 +4,71 @@ const Posts = Vue.component('posts', {
     <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
     Create Post
     </button>
-
     <button @click = "trigger_celery_job" > Trigger a celery task </button>
     <div class="row">
-        <div class="card my-4 mx-4 col-4" v-for="post in posts_data" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">{{post.title}}</h5>
-                <h6 class="card-subtitle mb-2 text-body-secondary">Post Id: {{post.id}}</h6>
-                <p class="card-text">{{post.content}}</p>
-                <button @click="delete_blog(post.id)" class="card-link">delete post</button>
-                <button type="button" class="card-link" :data-bs-target="'#editModal' + post.id" data-bs-toggle="modal">
-                Edit post
-                </button>
-
-    <!-- Create Post Modal -->
-    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+       <div class="card my-4 mx-4 col-4" v-for="post in posts_data" style="width: 18rem;">
+          <div class="card-body">
+             <h5 class="card-title">{{post.title}}</h5>
+             <h6 class="card-subtitle mb-2 text-body-secondary">Post Id: {{post.id}}</h6>
+             <p class="card-text">{{post.content}}</p>
+             <button @click="delete_blog(post.id)" class="card-link">delete post</button>
+             <button type="button" class="card-link" :data-bs-target="'#editModal' + post.id" data-bs-toggle="modal">
+             Edit post
+             </button>
+             <!-- Create Post Modal -->
+             <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                   <div class="modal-content">
+                      <div class="modal-header">
+                         <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body">
+                         <div class="my-3">
+                            <label for="title">Enter Post Title</label>
+                            <input v-model="title" type="text" id="title" class="form-control" placeholder="Title">
+                         </div>
+                         <div class="my-3">
+                            <label for="Content">Enter Post Content</label>
+                            <input v-model="content" type="text" id="Content" class="form-control" placeholder="Content">
+                         </div>
+                      </div>
+                      <div class="modal-footer">
+                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                         <button type="button" @click="addblog" class="btn btn-primary" data-bs-dismiss="modal">Submit</button>
+                      </div>
+                   </div>
                 </div>
-                <div class="modal-body">
-                    <div class="my-3">
-                        <label for="title">Enter Post Title</label>
-                        <input v-model="title" type="text" id="title" class="form-control" placeholder="Title">
-                    </div>
-                    <div class="my-3">
-                        <label for="Content">Enter Post Content</label>
-                        <input v-model="content" type="text" id="Content" class="form-control" placeholder="Content">
-                    </div>
+             </div>
+             <!-- Edit Post Modal -->
+             <div class="modal fade" :id="'editModal' + post.id" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="'editModalLabel' + post.id" aria-hidden="true">
+                <div class="modal-dialog">
+                   <div class="modal-content">
+                      <div class="modal-header">
+                         <h1 class="modal-title fs-5" :id="'editModal' + post.id">Edit Post</h1>
+                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body">
+                         <div class="my-3">
+                            <label for="title">Enter Post Title</label>
+                            <input v-model="title" type="text" id="title" class="form-control" :placeholder= "post.title">
+                         </div>
+                         <div class="my-3">
+                            <label for="Content">Enter Post Content</label>
+                            <input v-model="content" type="text" id="Content" class="form-control" :placeholder= "post.content">
+                         </div>
+                      </div>
+                      <div class="modal-footer">
+                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                         <button type="button" @click="editblog(post.id)" class="btn btn-primary" data-bs-dismiss="modal">Submit</button>
+                      </div>
+                   </div>
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" @click="addblog" class="btn btn-primary" data-bs-dismiss="modal">Submit</button>
-                </div>
-            </div>
-        </div>
+             </div>
+          </div>
+       </div>
     </div>
-
-    <!-- Edit Post Modal -->
-    <div class="modal fade" :id="'editModal' + post.id" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="'editModalLabel' + post.id" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" :id="'editModal' + post.id">Edit Post</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <div class="my-3">
-                        <label for="title">Enter Post Title</label>
-                        <input v-model="title" type="text" id="title" class="form-control" :placeholder= "post.title">
-                    </div>
-                    <div class="my-3">
-                        <label for="Content">Enter Post Content</label>
-                        <input v-model="content" type="text" id="Content" class="form-control" :placeholder= "post.content">
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" @click="editblog(post.id)" class="btn btn-primary" data-bs-dismiss="modal">Submit</button>
-                </div>
-            </div>
-        </div>
-    </div>
-    </div>
-        </div>
-    </div>
-
-
-</div>
+ </div>
 `,
 data: function() {
     return {
